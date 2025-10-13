c6d8c5d02ce052da3e933e157db8cb859a9ce05f
Сенчик <arseniytyurin@mail.ru>
2025-10-13 21:29:49 +0300

    markup

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SnakeGame
+{
+    static class Program
+    {
+        // константы, переменные и списки
+        const int WIDTH = 20;
+        const int HEIGHT = 20;
+
+        static Random rnd = new Random();
+
+        static List<(int sx, int sy)> snake = new List<(int sx, int sy)>();
+        static (int fx, int fy) food;
+
+
+        // очистка консоли
+        static void ClearConsole()
+        {
+
+        }
+
+        // движение змейки
+        static void SnakeMove()
+        {
+            
+        }
+
+        // появление еды на карте
+        static void SpawnFood()
+        {
+
+        }
+
+        // считывание с клавиатуры
+        static void ReadInput()
+        {
+
+        }
+
+        // отрисовка всего
+        static void Draw()
+        {
+
+        }
+
+        static void Main()
+        {
+            
+        }
+    }
+}
