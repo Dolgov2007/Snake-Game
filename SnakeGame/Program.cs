956e9c385fdc3597df195ca482a93df8c67ba1b3
Сенчик <arseniytyurin@mail.ru>
2025-10-13 22:02:31 +0300

    add Draw and SpawnFood

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -4,7 +4,7 @@ namespace SnakeGame
 {
     static class Program
     {
-        // константы, переменные и списки
+        // данные
         const int WIDTH = 20;
         const int HEIGHT = 20;
 
@@ -16,6 +16,12 @@ namespace SnakeGame
 
         // очистка консоли
         static void ClearConsole()
+        {
+            Console.Clear();
+        }
+
+        // считывание с клавиатуры
+        static void ReadInput()
         {
 
         }
@@ -29,24 +35,76 @@ namespace SnakeGame
         // появление еды на карте
         static void SpawnFood()
         {
+            while (true)
+            {
+                int fx = rnd.Next(2, WIDTH);
+                int fy = rnd.Next(2, HEIGHT);
 
-        }
+                bool onSnake = false;
+                foreach (var part in snake)
+                {
+                    if (fx == part.sx && fy == part.sy)
+                    {
+                        onSnake = true;
+                        break;
+                    }
+                }
 
-        // считывание с клавиатуры
-        static void ReadInput()
-        {
-
+                if (!onSnake)
+                {
+                    food = (fx, fy);
+                    break;
+                }
+            }
         }
 
         // отрисовка всего
         static void Draw()
         {
+            ClearConsole();
 
+            for (int y = 0; y < HEIGHT; y++)
+            {
+                for (int x = 0; x < WIDTH; x++)
+                {
+                    if (y == 0 || y == HEIGHT - 1)
+                        Console.Write("#");
+                    else
+                    {
+                        if (x == 0 || x == WIDTH - 1)
+                            Console.Write("#");
+                        else if ((x, y) == snake[0])
+                            Console.Write("O");
+                        else if (x == food.fx && y == food.fy)
+                            Console.Write("@");
+                        else
+                            Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        static void CheckWin()
+        {
+            if (snake.Count == (WIDTH - 1) * (HEIGHT - 1))
+            {
+                return;
+            }
         }
 
         static void Main()
         {
-            
+            snake.Clear();
+            snake.Add((WIDTH / 2, HEIGHT / 2));
+
+            SpawnFood();
+
+            while (true)
+            {
+                Draw();
+                Thread.Sleep(100);
+            }
         }
     }
 }
